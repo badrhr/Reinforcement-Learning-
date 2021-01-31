2cb7bcab254e05f7c6acfe21790f64efffca432b
Xproce <badr.hirchoua@gmail.com>
2021-01-31 18:32:21 +0100

    Update test.py

--- test.py
+++ test.py
@@ -7,9 +7,7 @@ Created on Sat Jan 30 02:28:52 2021
 
 from Env import DQNAgent
 
-if __name__ == "__main__":
-    agent = DQNAgent()
-    agent.run()
-
+agent = DQNAgent()
+agent.run()
 
 agent.test()
